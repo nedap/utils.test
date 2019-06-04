7d19a73556df7641d81ae696d1d698c6d97ddac6
Jeroen de Jong <jeroen.dejong@nedap.com>
2019-06-04 09:19:57 +0200

    Bump version to 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.nedap.staffing-solutions/utils.test "1.0.0-SNAPSHOT"
+(defproject com.nedap.staffing-solutions/utils.test "1.1.1"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[org.clojure/clojure "1.10.0"]]
 
