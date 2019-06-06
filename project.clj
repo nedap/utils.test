799b312724a62c59d7d6428cf0455bfeea191f2c
vemv <vemv@users.noreply.github.com>
2019-06-06 03:11:08 +0200

    v1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.nedap.staffing-solutions/utils.test "1.1.1"
+(defproject com.nedap.staffing-solutions/utils.test "1.2.0"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[org.clojure/clojure "1.10.0"]]
 
