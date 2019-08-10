0ef24f2fe710cce1f23e76e005b7ae2c657038d1
vemv <vemv@users.noreply.github.com>
2019-08-10 19:33:11 +0200

    v1.5.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject com.nedap.staffing-solutions/utils.test "1.4.0"
+(defproject com.nedap.staffing-solutions/utils.test "1.5.0"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[org.clojure/clojure "1.10.1"]]
 
