18fed9d1f2f5142aedd4a2bf7ece05575c74f7d9
vemv <vemv@users.noreply.github.com>
2019-12-31 03:57:16 +0100

    Reapply our Lein template

--- project.clj
+++ project.clj
@@ -59,14 +59,14 @@
                                        [com.nedap.staffing-solutions/utils.spec.predicates "1.1.0"]
                                        [com.stuartsierra/component "0.4.0"]
                                        [com.taoensso/timbre "4.10.0"]
-                                       [criterium "0.4.4"]
-                                       [formatting-stack "1.0.0"]
+                                       [criterium "0.4.5"]
+                                       [formatting-stack "1.0.1"]
                                        [lambdaisland/deep-diff "0.0-29"]
                                        [medley "1.2.0"]
                                        [org.clojure/core.async "0.5.527"]
                                        [org.clojure/math.combinatorics "0.1.1"]
                                        [org.clojure/test.check "0.10.0-alpha3"]
-                                       [org.clojure/tools.namespace "0.3.0-alpha4"]]
+                                       [org.clojure/tools.namespace "0.3.1"]]
                         :plugins      [[lein-cloverage "1.1.1"]]
                         :source-paths ["dev"]
                         :repl-options {:init-ns dev}}
@@ -75,14 +75,19 @@
                                         :exclusions [com.cognitect/transit-clj
                                                      com.google.code.findbugs/jsr305
                                                      com.google.errorprone/error_prone_annotations]]
+                                       [com.google.guava/guava "25.1-jre" #_"transitive"]
+                                       [com.google.protobuf/protobuf-java "3.4.0" #_"transitive"]
                                        [com.cognitect/transit-clj "0.8.313" #_"transitive"]
                                        [com.google.errorprone/error_prone_annotations "2.1.3" #_"transitive"]
                                        [com.google.code.findbugs/jsr305 "3.0.2" #_"transitive"]]}
 
-             :test     {:dependencies [[com.nedap.staffing-solutions/matcher-combinators "1.1.0-alpha1"
-                                        :exclusions [commons-codec]]
-                                       [com.nedap.staffing-solutions/utils.test "1.6.1"]]
-                        :jvm-opts     ["-Dclojure.core.async.go-checking=true"]}
+             :check    {:global-vars  {*unchecked-math* :warn-on-boxed
+                                       ;; avoid warnings that cannot affect production:
+                                       *assert*         false}}
+
+             :test     {:dependencies [[com.nedap.staffing-solutions/utils.test "1.6.1"]]
+                        :jvm-opts     ["-Dclojure.core.async.go-checking=true"
+                                       "-Duser.language=en-US"]}
 
              :ci       {:pedantic?    :abort
                         :jvm-opts     ["-Dclojure.main.report=stderr"]
