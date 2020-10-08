c5ae8e26b1f120e1d73e2ff2539f69ee1ac2c963
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-10-08 14:38:50 +0200

    v1.8.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject com.nedap.staffing-solutions/utils.test "1.7.0"
+(defproject com.nedap.staffing-solutions/utils.test "1.8.0"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[org.clojure/clojure "1.10.1"]]
 
